10487daed8a02e13f81f2a371ff751b19ddab0b2
立入 <88256484+i-tachiiri@users.noreply.github.com>
2025-06-21 09:13:10 +0900

    Add simple answer list and CSV export

--- src/Api/Functions/AnswersFunctions.cs
+++ src/Api/Functions/AnswersFunctions.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using System.IO;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -12,6 +14,7 @@ public class AnswersFunctions
 {
     private readonly ILogger _logger;
     private readonly IEncryptionService _encryptionService;
+    private static readonly List<string> _answers = new();
 
     public AnswersFunctions(ILogger<AnswersFunctions> logger, IEncryptionService encryptionService)
     {
@@ -27,6 +30,7 @@ public class AnswersFunctions
         _logger.LogInformation("PostAnswer called");
         var body = await new StreamReader(req.Body).ReadToEndAsync();
         var encrypted = _encryptionService.Encrypt(body);
+        _answers.Add(encrypted);
         var res = req.CreateResponse(HttpStatusCode.OK);
         res.WriteString(encrypted);
         return res;
@@ -39,7 +43,21 @@ public class AnswersFunctions
     {
         _logger.LogInformation("GetAnswers called");
         var res = req.CreateResponse(HttpStatusCode.OK);
-        res.WriteString("answers list");
+        var list = string.Join("\n", _answers);
+        res.WriteString(list);
+        return res;
+    }
+
+    [Function("GetAnswersCsv")]
+    public HttpResponseData GetAnswersCsv([
+        HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "answers/csv")]
+        HttpRequestData req)
+    {
+        _logger.LogInformation("GetAnswersCsv called");
+        var res = req.CreateResponse(HttpStatusCode.OK);
+        var csv = string.Join("\n", _answers.Select(a => $"\"{a}\""));
+        res.Headers.Add("Content-Disposition", "attachment; filename=answers.csv");
+        res.WriteString(csv);
         return res;
     }
 }
